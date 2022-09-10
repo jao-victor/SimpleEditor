cfc9aeace7377803025cf66a9df4d812534c91b9
João Victor <ijoaovictor76@gmail.com>
2022-09-10 17:06:50 -0400

    update
    Principais Métodos implantados
    Editor já útilizável

---
+++ .py
@@ -0,0 +1,1 @@
+print("Em fim Pythonoubhljmklç,")
--- editor.py
+++ editor.py
@@ -1,33 +1,96 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 import sys
-from  classEditor import Ui_MainWindow
+from  classEditor2 import Ui_SimpleEditor
 from PyQt5.QtWidgets import QMainWindow
+import os
 
 class Editor(QMainWindow):
 
 	def __init__(self):
 		super().__init__()
-		self.editor = Ui_MainWindow()
+		self.editor = Ui_SimpleEditor()
 		self.editor.setupUi(self)
-
-		self.editor.actionSalvar_como_2.triggered.connect(self.salvar)
+		self.editor.actionSalvar_como.triggered.connect(self.salvar)
 		self.editor.actionAbrir.triggered.connect(self.abrir)
+		self.editor.rodar.clicked.connect(self.executar)
+		self.editor.actionPython.triggered.connect(self.python)
+		self.editor.actionC.triggered.connect(self.C)
+		self.editor.actionJava.triggered.connect(self.java)
+
+		self.caminho = ' '
+		self.comp = 'python3'
+		self.ext = '.py'
+
+
+	def python(self):
+		import platform
+		self.ext = '.py'
+		if (platform.system() == "Windows"):
+			self.comp = "python"
+		else:
+			self.comp = "python3"
+
+	def java(self):
+		self.ext = '.java'
+
+
+	def C (self):
+		self.ext = '.c'
+
 
 	def salvar(self):
 
-		caminho = QtWidgets.QFileDialog.getSaveFileName()[0]
+		self.caminho = QtWidgets.QFileDialog.getSaveFileName()[0]
 		self.codigo = self.editor.areaCodigo.toPlainText() #captura o conteúdo do editor
 
-		with open(caminho + '.py', 'w') as file:
-			file.write(self.codigo)
-			file.close()
+		if(self.ext == '.py'):
+			with open(self.caminho + '.py', 'w') as file:
+				file.write(self.codigo)
+				self.file = file
+				file.close()
+
+		if (self.ext == '.java'):
+			with open(self.caminho + '.java', 'w') as file:
+				file.write(self.codigo)
+				self.file = file
+				file.close()
+
+		if (self.ext == '.c'):
+			with open(self.caminho + '.c', 'w') as file:
+				file.write(self.codigo)
+				self.file = file
+				file.close()
+
 
 	def abrir(self):
 
-		caminho = QtWidgets.QFileDialog.getOpenFileName()[0]
-		with open(caminho, 'r') as arquivo:
+		self.caminho = QtWidgets.QFileDialog.getOpenFileName()[0]
+		with open(self.caminho, 'r') as arquivo:
 			self.codigo = arquivo.read()
 			self.editor.areaCodigo.setPlainText(self.codigo)
+
+
+	def executar(self):
+
+
+		if(self.caminho != ' ' and self.ext == '.java'):
+			os.system(f'javac {self.caminho}.java')
+			os.system(f'java {self.caminho.split("/")[-1]}')
+
+		elif(self.caminho != ' ' and self.ext == '.c'):
+			os.system(f'gcc {self.caminho}.c -o {self.caminho.split("/")[-1]}')
+			os.system(f'./{self.caminho.split("/")[-1]}')
+
+		elif(self.caminho != ' ' and self.ext == '.py'):
+			os.system(f'{self.comp} {self.caminho}.py')
+
+		else:
+			msg = QMessageBox()
+			msg.setWindowTitle("WARNING")
+			msg.setText("Salve seu arquivo")
+			msg.setIcon(QMessageBox.Critical)
+			msg.exec()
 
 
 
